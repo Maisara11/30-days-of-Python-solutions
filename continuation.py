14135fa4e7618491685aef0a2cd81b624b898146
Maisara11 <maisaralweno6@gamil.com>
2026-08-18 21:47:54 +0300

    Day 2 exercise

---
+++ continuation.py
@@ -0,0 +1,4 @@
+import math 
+point_1 = (2,3)
+point_2 = (6,2)
+print(math.dist(point_1,point_2))
--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,6 @@
 print('hello world!') 
 """xxjgsdyurshsxhgfdfuwkdufdshfckdhcdksgdhafshsfdtshgfxcdkgkdgcdc"""
 #single line comment the above was supposed to illustrate a multliine comment#
+point_1 = (2,3)
+point_2 = (6,2)
+print(Math.distance(point_1,point_2))
